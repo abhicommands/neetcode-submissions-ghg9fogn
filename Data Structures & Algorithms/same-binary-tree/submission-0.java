d3620401b9904e78963a57451851bae877d1a013
Abhinav Acharya <aa2372@scarletmail.rutgers.edu>
2026-06-07 14:40:57 -0700

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    // iterative
+    // public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+    // }
+    //recursive:
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        else if ((p==null && q!=null) || (p!=null && q==null)) return false;
+        else if(p.val != q.val) return false;
+        else return (isSameTree(q.left, p.left) && (isSameTree(q.right, p.right)));
+    }
+
+}
