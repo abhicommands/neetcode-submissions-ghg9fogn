fd6976ccb6cbcf4fc92552dc3afb9f7affb19288
Abhinav Acharya <aa2372@scarletmail.rutgers.edu>
2026-06-12 22:49:10 -0700

    Add: binary-tree-postorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-3.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     class Visited {
+    //         TreeNode node;
+    //         boolean visited;
+    //         Visited() {}
+    //         Visited(TreeNode node1, boolean val) {
+    //             node = node1;
+    //             visited = val;
+    //         }
+    //     }
+    //     List<Integer> res = new ArrayList<Integer>();
+    //     if (root == null)
+    //         return res;
+    //     Deque<Visited> stack = new ArrayDeque<>();
+    //     stack.push(new Visited(root, false));
+    //     while (!stack.isEmpty()) {
+    //         Visited tNode = stack.peek();
+    //         if (tNode.visited) {
+    //             res.add(stack.pop().node.val);
+    //         } else {
+    //             tNode.visited = true;
+    //             if(tNode.node.right!=null) stack.push(new Visited(tNode.node.right, false));
+    //             if(tNode.node.left!=null) stack.push(new Visited(tNode.node.left, false));
+    //         }
+    //     }
+    //     return res;
+    // }
+    //recursive
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root==null){
+            return res;
+        }
+        helper(root, res);
+        return res;
+    }
+    public void helper(TreeNode root, List<Integer> res) {
+        if(root!=null) {
+            helper(root.left, res);
+            helper(root.right, res);
+            res.add(root.val);
+        }
+        else return;
+    }
+}
