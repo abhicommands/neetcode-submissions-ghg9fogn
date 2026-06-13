a5643a176dbe76b5205c83dc370fa92b5c7e8e59
Abhinav Acharya <aa2372@scarletmail.rutgers.edu>
2026-06-12 19:26:40 -0700

    Add: binary-tree-inorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-3.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        helper(root, res);
+        return res;
+    }
+    // recursive
+    //  public void helper(TreeNode root, List<Integer> res) {
+    //      if(root.left!=null) helper(root.left, res);
+    //      res.add(root.val);
+    //      if(root.right!=null) helper(root.right, res);
+    //  }
+    // iterative
+    public void helper(TreeNode root, List<Integer> res) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+                res.add(curr.val);
+                curr= curr.right;
+            }
+        }
+    }
+}
