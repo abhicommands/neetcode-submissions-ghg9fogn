7e31bbb843a13e6910dfe6716bc610dc9614ac0a
Abhinav Acharya <aa2372@scarletmail.rutgers.edu>
2026-09-05 11:59:27 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        int[] list = new int[26];
+        for (int i =0; i < s.length(); i++) {
+            list[s.charAt(i)-'a']++;
+            list[t.charAt(i)-'a']--;
+        }
+        for(int x : list) {
+            if( x != 0)
+            return false;
+        }
+        return true;
+    }
+}
